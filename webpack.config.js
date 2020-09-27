4939cc535b4c2a5a440bdb4b0dfa2ad5a105546f
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-09-27 12:31:40 -0300

    slint

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HtmlWebPackPlugin= require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports={
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        open: true,
+        hot: true,
+        port: 8081
+    },
+    mode: 'development',
+    resolve:{
+        extensions: ['.js', '.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use:[
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(s*)css$/,
+                use:[
+                    {loader: MiniCssExtractPlugin.loader},
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+
+        }),
+        new MiniCssExtractPlugin({
+            filename:'assets/[name].css'
+        }),
+    ]
+}
