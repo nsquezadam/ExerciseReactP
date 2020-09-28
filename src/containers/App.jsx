ccb3ef8198d07a4be18bc168f2480eabb5949e88
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-09-28 10:56:27 -0300

    se  agrega proptypes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,43 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import Header from '../components/Header';
-import '../assets/styles/App.scss'
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
+import '../assets/styles/App.scss';
+
+const API = 'http://localhost:3000/initalState';
 const App = () => {
+ const InitialState = useInitialState(API);
   return (
     <div className='App'>
       <Header />
+      <Search />
+
+      {InitialState.myList?.length > 0 && (
+        <Categories title='Mi Lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title='Tendencias'>
+        <Carousel>
+          {InitialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+
+        </Carousel>
+      </Categories>
+      <Categories title='Originales de Platzi Video'>
+        <Carousel>
+          {InitialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Footer />
     </div>
   );
 };
